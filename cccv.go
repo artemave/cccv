10a6187dd6bdfd6a3b3eb634eca0c7c66816b936
artemave <artemave@gmail.com>
2014-07-23 22:30:05 +0100

    preparing to actually write some tests

--- cccv.go
+++ cccv.go
@@ -3,59 +3,72 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/AndrewVos/o"
 )
 
-type ChangedLine struct {
-	FileName string
-	Text     string
+type FileName string
+
+type Change struct {
+	FileName
+	Line
+}
+
+type Line struct {
+	Number int
+	Text   string
+}
+
+type FileResult struct {
+	FileName
+	Lines []*Line
 }
 
 func main() {
-	result := make(map[string][]string)
-	changedLineItselfAlreadySkipped := make(map[ChangedLine]bool)
+	results := []FileResult{}
 
-	changes := getChanges()
+	changes := getChanges(os.Stdin)
 	allGitFiles := gitLsFiles()
 
 	for _, fName := range allGitFiles {
-		file, _ := os.Open(fName)
-		scanner := bufio.NewScanner(file)
-		uniqLines := make(map[string]bool)
+		r := GenResultForFile(fName, changes)
+		results = append(results, r)
+	}
 
-		for scanner.Scan() {
-			line := scanner.Text()
-			trimmedLine := strings.TrimSpace(line)
+	o.O(results)
+}
 
-			if uniqLines[trimmedLine] {
-				continue
-			}
-			uniqLines[trimmedLine] = true
+func GenResultForFile(fName string, changes *[]*Change) FileResult {
+	file, _ := os.Open(fName)
+	scanner := bufio.NewScanner(file)
+	currentLineNumber := 0
+	result := FileResult{FileName: FileName(fName), Lines: []*Line{}}
 
-			for _, changedLine := range *changes {
-				if changedLine.Text == trimmedLine {
+	for scanner.Scan() {
+		line := scanner.Text()
+		trimmedLine := strings.TrimSpace(line)
+		currentLineNumber++
 
-					if changedLine.FileName == fName && !changedLineItselfAlreadySkipped[*changedLine] {
-						changedLineItselfAlreadySkipped[*changedLine] = true
-						continue
-					}
+		for _, changedLine := range *changes {
+			if changedLine.Text == trimmedLine {
 
-					if result[fName] == nil {
-						result[fName] = []string{}
-					}
-					result[fName] = append(result[fName], line)
+				// exclude lines from the diff itself
+				if string(changedLine.FileName) == fName && changedLine.Line.Number == currentLineNumber {
+					continue
 				}
+
+				result.Lines = append(result.Lines, &Line{Number: currentLineNumber, Text: line})
 			}
 		}
 	}
-
-	o.O(result)
+	return result
 }
 
 func gitLsFiles() []string {
@@ -80,26 +93,44 @@ func gitLsFiles() []string {
 	return files
 }
 
-func getChanges() *[]*ChangedLine {
-	scanner := bufio.NewScanner(os.Stdin)
+func getChanges(reader io.Reader) *[]*Change {
+	scanner := bufio.NewScanner(reader)
 	var currentFile string
-	changes := &[]*ChangedLine{}
+	var currentLineNumber int
+	changes := &[]*Change{}
 
 	currentFileR := regexp.MustCompile(`^\+\+\+ ./(.*)$`)
 	lineAddedR := regexp.MustCompile(`^\+{1}(.*\w+.*)`)
+	lineRangeR := regexp.MustCompile(`^@@.*?\+(\d+?),`)
 
 	for scanner.Scan() {
 		currentLine := scanner.Text()
 
-		if currentFileR.MatchString(currentLine) {
-			res := currentFileR.FindStringSubmatch(currentLine)
+		if res := currentFileR.FindStringSubmatch(currentLine); res != nil {
 			currentFile = res[1]
+
+		} else if res := lineRangeR.FindStringSubmatch(currentLine); res != nil {
+			r, err := strconv.Atoi(res[1])
+			if err != nil {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+			currentLineNumber = r
+
 		} else if lineAddedR.MatchString(currentLine) == true {
 			res := lineAddedR.FindStringSubmatch(currentLine)
-			newChange := &ChangedLine{FileName: currentFile, Text: strings.TrimSpace(res[1])}
+			newChange := &Change{
+				FileName: FileName(currentFile),
+				Line:     Line{Text: strings.TrimSpace(res[1]), Number: currentLineNumber},
+			}
 			*changes = append(*changes, newChange)
+			currentLineNumber++
+
+		} else {
+			currentLineNumber++
 		}
 	}
+
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		os.Exit(1)
